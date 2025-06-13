1607a5650164c6df167a0605d93e78b20b364682
Greg Stoll <greg@gregstoll.com>
2025-06-13 16:09:28 -0500

    cleanup, share resultView, add restart

--- wator_simulation.js
+++ wator_simulation.js
@@ -1,46 +1,7 @@
-var statusElement = document.getElementById('status');
-var spinnerElement = document.getElementById('spinner');
 var canvasElement = document.getElementById('canvas');
-var outputElement = document.getElementById('output');
-if (outputElement) outputElement.value = ''; // clear browser cache
 
 var Module = {
-print(...args) {
-    console.log(...args);
-    // These replacements are necessary if you render to raw HTML
-    //text = text.replace(/&/g, "&amp;");
-    //text = text.replace(/</g, "&lt;");
-    //text = text.replace(/>/g, "&gt;");
-    //text = text.replace('\n', '<br>', 'g');
-    if (outputElement) {
-    var text = args.join(' ');
-    outputElement.value += text + "\n";
-    outputElement.scrollTop = outputElement.scrollHeight; // focus on bottom
-    }
-},
 canvas: canvasElement,
-setStatus(text) {
-    Module.setStatus.last ??= { time: Date.now(), text: '' };
-    if (text === Module.setStatus.last.text) return;
-    var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
-    var now = Date.now();
-    // if this is a progress update, skip it if too soon
-    if (m && now - Module.setStatus.last.time < 30) return;
-    Module.setStatus.last.time = now;
-    Module.setStatus.last.text = text;
-    if (m) {
-    text = m[1];
-    spinnerElement.hidden = false;
-    } else {
-    if (!text) spinnerElement.style.display = 'none';
-    }
-    statusElement.innerHTML = text;
-},
-totalDependencies: 0,
-monitorRunDependencies(left) {
-    this.totalDependencies = Math.max(this.totalDependencies, left);
-    Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies-left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
-},
 IMAGES: [],
 initImages() {
     let promises = [];
@@ -56,23 +17,26 @@ initImages() {
     }
     return Promise.all(promises);
 },
-async onRuntimeInitialized() {
+resultView: undefined,
+startWator() {
     this._init_wator(15, 15);
-    await this.initImages();
+    let data = this._getCellData();
+    resultView = new Uint8Array(Module.HEAPU8.buffer, data, 15 * 15 * 3);
     this.render();
+},
+async onRuntimeInitialized() {
+    await this.initImages();
+    this.startWator();
 },
 CELL_SIZE: 40,
 COLOR_NAMES: ['red', 'green', 'blue', 'yellow', 'cyan', 'magenta', 'gray', 'orange'],
 render() {
-    let data = this._getCellData();
-    const resultView = new Uint8Array(Module.HEAPU8.buffer, data, 15 * 15 * 3);
     /** @type CanvasRenderingContext2D */
     let ctx = this.canvas.getContext('2d');
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    // TODO?
-    /*ctx.imageSmoothingEnabled = true;
-    ctx.imageSmoothingQuality = "high";*/
+    ctx.imageSmoothingEnabled = true;
+    ctx.imageSmoothingQuality = "high";
     let tempCanvas = document.createElement("canvas");
     tempCanvas.width = this.CELL_SIZE;
     tempCanvas.height = this.CELL_SIZE;
@@ -117,13 +81,4 @@ tick() {
     this._draw_wator();
     this.render();
 },
-};
-Module.setStatus('Downloading...');
-window.onerror = (event) => {
-// TODO: do not warn on ok events like simulating an infinite loop or exitStatus
-Module.setStatus('Exception thrown, see JavaScript console');
-spinnerElement.style.display = 'none';
-Module.setStatus = (text) => {
-    if (text) console.error('[post-exception status] ' + text);
-};
 };
